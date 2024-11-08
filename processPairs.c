3c124f0d901ef706666a882c717808c454c49981
visand <visand@kth.se>
2024-11-08 10:39:57 +0100

    First fork test

--- processPairs.c
+++ processPairs.c
@@ -1,11 +1,26 @@
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 
 int main() {
 
-    printf("First commit things");
+    const char *name = "/tmp/myfifo";
+    const char *wbuf = "Hello";
+    int fd;
+
+    mkfifo(name, 0666);
+
+    fd = open(name, O_WRONLY);
+    write(fd, wbuf, strlen(wbuf) + 1);
+    close(fd);
+    unlink(name);
+
+    printf("This is a process");
+    fork();
     return 0;
 
 }
