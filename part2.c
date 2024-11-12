0ebbd13d134e7fbe7107559a2e617ba2adae6e9b
pottsorken <163292800+pottsorken@users.noreply.github.com>
2024-11-12 13:47:12 +0100

    bench

--- part2.c
+++ part2.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#define N 2048
+#define N 100
 #define NTHRD 4
 
 // For timekeeping
@@ -31,42 +31,89 @@ void *partial_sum(void *p) {
         args->psum += a[i];
 }
 
+// int main() {
+//     pthread_t thrd[NTHRD];
+//     args_t thrd_args[NTHRD];
+
+//     // Create input array
+//     a = (double *) malloc(N * sizeof(double));
+//     for (int i = 0; i < N; i++) 
+//         a[i] = i;
+
+//     clock_gettime(CLOCK_MONOTONIC, &t_start);
+//     // Create threads
+//     for (int i = 0; i < NTHRD; i++) {
+//         thrd_args[i].imin = i * N /NTHRD;
+//         thrd_args[i].imax = (i + 1) * N /NTHRD;
+//         if (pthread_create(&thrd[i], NULL, partial_sum, &thrd_args[i])) {
+//             perror("pthread_create");
+//             _exit(1);
+//         }
+//     }
+
+//     // Wait for threads to finish
+//     for (int i = 0; i < NTHRD; i++)
+//         if (pthread_join(thrd[i], NULL)) {
+//             perror("pthread_join");
+//             _exit(1);
+//         }
+
+//     // Compute global sum
+//     double gsum = 0.0;
+//     for(int i = 0; i < NTHRD; i++)
+//         gsum += thrd_args[i].psum;
+
+//     clock_gettime(CLOCK_MONOTONIC, &t_stop);
+//     measured_time = nano_seconds(&t_start, &t_stop);
+
+//     printf("gsum = %.1f\n", gsum);
+//     printf("Threads: %d , Length: %d\n", NTHRD, N);
+//     printf("Result obtained in %lld ns\n---------------------\n", measured_time);
+// }
+
 int main() {
-    pthread_t thrd[NTHRD];
-    args_t thrd_args[NTHRD];
+    int length = N;
 
-    // Create input array
-    a = (double *) malloc(N * sizeof(double));
-    for (int i = 0; i < N; i++) 
-        a[i] = i;
+    for (int j = 0; j < 6; j++) {
+        pthread_t thrd[NTHRD];
+        args_t thrd_args[NTHRD];
+        double gsum = 0.0;
 
-    clock_gettime(CLOCK_MONOTONIC, &t_start);
-    // Create threads
-    for (int i = 0; i < NTHRD; i++) {
-        thrd_args[i].imin = i * N /NTHRD;
-        thrd_args[i].imax = (i + 1) * N /NTHRD;
-        if (pthread_create(&thrd[i], NULL, partial_sum, &thrd_args[i])) {
-            perror("pthread_create");
-            _exit(1);
+        // Create input array
+        a = (double *) malloc(length * sizeof(double));
+        for (int i = 0; i < length; i++) 
+            a[i] = i;
+
+        clock_gettime(CLOCK_MONOTONIC, &t_start);
+
+        for (int k = 0; k < 100; k++) {
+            // Create threads
+            for (int i = 0; i < NTHRD; i++) {
+                thrd_args[i].imin = i * length /NTHRD;
+                thrd_args[i].imax = (i + 1) * length /NTHRD;
+                if (pthread_create(&thrd[i], NULL, partial_sum, &thrd_args[i])) {
+                    perror("pthread_create");
+                    _exit(1);
+                }
+            }
+
+            // Wait for threads to finish
+            for (int i = 0; i < NTHRD; i++)
+                if (pthread_join(thrd[i], NULL)) {
+                    perror("pthread_join");
+                    _exit(1);
+                }
+
+            // Compute global sum
+            for(int i = 0; i < NTHRD; i++)
+                gsum += thrd_args[i].psum;
         }
+
+        clock_gettime(CLOCK_MONOTONIC, &t_stop);
+        measured_time = nano_seconds(&t_start, &t_stop);
+
+        printf("gsum = %.1f\n", gsum);
+        printf("Threads: %d , Length: %d\n", NTHRD, length);
+        printf("Result obtained in %lld ns\n---------------------\n", measured_time/100);
     }
-
-    // Wait for threads to finish
-    for (int i = 0; i < NTHRD; i++)
-        if (pthread_join(thrd[i], NULL)) {
-            perror("pthread_join");
-            _exit(1);
-        }
-
-    // Compute global sum
-    double gsum = 0.0;
-    for(int i = 0; i < NTHRD; i++)
-        gsum += thrd_args[i].psum;
-
-    clock_gettime(CLOCK_MONOTONIC, &t_stop);
-    measured_time = nano_seconds(&t_start, &t_stop);
-
-    printf("gsum = %.1f\n", gsum);
-    printf("Threads: %d , Length: %d\n", NTHRD, N);
-    printf("Result obtained in %lld ns\n---------------------\n", measured_time);
 }
