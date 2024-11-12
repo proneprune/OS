81fe43c60a86896fd642c1c9f29c003cb230bb3d
visand <visand@kth.se>
2024-11-12 20:03:40 +0100

    First draft

--- masterWorkers.c
+++ masterWorkers.c
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <time.h>
 
+#define STACK_DEFAULT_SIZE 4
+
 typedef struct child_stack {
 
     int *PIDs;
@@ -28,6 +30,26 @@ typedef struct msg_queue {
     msg_node *last;
 
 } msg_queue;
+
+typedef struct family {
+
+    msg_queue *queue;
+    child_stack *stack;
+
+} family;
+
+void print_msg_queue(msg_queue *q) {
+
+    msg_node *t = q->first;
+
+    while(t != NULL) {
+
+        printf("%s", t->msg);
+        t = t->next;
+
+    }
+
+}
 
 child_stack *create_child_stack(int length) {
 
@@ -62,10 +84,10 @@ child_stack *extend_child_stack(child_stack *cs, int new_length) {
 child_stack *add_to_child_stack(child_stack *cs, int PID) {
 
     if(cs == NULL)
-        cs = create_child_stack(4);
+        cs = create_child_stack(STACK_DEFAULT_SIZE);
 
     if(cs->SP == cs->length)
-        cs = extend_child_stack(cs, cs->length + 4);
+        cs = extend_child_stack(cs, cs->length + STACK_DEFAULT_SIZE);
 
     cs->PIDs[cs->SP++] = PID;
 
@@ -123,6 +145,25 @@ msg_queue *enqueue_msg(msg_queue *q, msg_node *m) {
         
 }
 
+family *create_family() {
+
+    family *new = (family*)malloc(sizeof(family));
+    new->queue = create_msg_queue();
+    new->stack = create_child_stack(STACK_DEFAULT_SIZE);
+
+    return new;
+
+}
+
+void send_message(msg_queue *q, char *msg, int pid, int rec) {
+
+    msg_node* m = create_msg_node();
+    m->msg = "[%d] %s", pid, msg;
+    m->receiver = rec; // send to everyone
+    q = enqueue_msg(q, m);
+
+}
+
 // TODO message queue to send/receive to every family
 // int as intended receiver and switch case on negative
 // to specify all/parent/error
@@ -132,9 +173,12 @@ int main() {
     printf("PROGRAM START\n");
 
     int iterations = 4;
-    child_stack *cs = NULL;
+    family *f = create_family();
+    f->queue = create_msg_queue();
+    f->stack = create_child_stack(STACK_DEFAULT_SIZE);
     int child_pid = -1;
-    int my_pid = getpid();
+    int my_pid = -1;
+    int root_pid = getpid();
 
     for(int i = 0; i < iterations; i++) {
 
@@ -144,21 +188,26 @@ int main() {
         if(child_pid == -1) // error
             printf("[%d] FORK ERROR\n", my_pid);
 
-        if(child_pid == 0) { // child
+        else if(child_pid == 0) { // child
 
             printf("[%d] Child process.\n", my_pid);
-            cs = create_child_stack(4);
+            f->queue = add_to_child_stack(f->queue, child_pid);
 
-        } else { // parent
-
+        } else // parent
       	    printf("[%d] Parent process with child child_pid: %d\n", my_pid, child_pid);
-            cs = add_to_child_stack(cs, child_pid);
-
-	    }
 
     }
 
-    printf("[%d] Child count: %d\n", my_pid, cs->SP);
+    if(my_pid == root_pid) {
+
+        printf("[%d] Child count: %d\n", my_pid, f->stack->SP);
+        send_message(f->queue, "Hello this is the parent process!", my_pid, -2);
+        sleep(25);
+        print_msg_queue(f->queue);
+
+    } else
+        send_message(f->queue, "This is a child.", my_pid, 0);
+
     printf("[%d] PROCESS END\n", my_pid);
     return 0;
 
