cf6b4b466b6c087b0ad254a27101dc1657871fc9
pottsorken <163292800+pottsorken@users.noreply.github.com>
2024-11-22 14:11:39 +0100

    Added benchmarking

--- Lab 2/semaphore.c
+++ Lab 2/semaphore.c
@@ -4,7 +4,19 @@
 #include <sys/stat.h>
 #include <pthread.h>
 #include <stdatomic.h>
-// #include <stdbool.h>
+
+// ---------- TIMEKEEPING ----------
+#include <sys/time.h>
+#include <time.h>
+
+double seconds(struct timespec* t_start, struct timespec* t_stop) {
+    return (t_stop->tv_sec - t_start->tv_sec) + 
+           (t_stop->tv_nsec - t_start->tv_nsec) / 1e9;
+}
+
+struct timespec t_start, t_stop;
+double measured_time;
+// ---------- END OF TIMEKEEPING ----------
 
 int counter = 0;
 const int nthr = 1000;
@@ -27,9 +39,9 @@ int sem_wait(mysemaphore_t* s) {
     /* FUNCTION TO BE IMPLEMENTED */
     int delay = 100;
     while (atomic_flag_test_and_set(&s->flg)) {
-        usleep(delay);
-        delay *= 2;  
-        if (delay > 1000) delay = 1000; // Limit delay to 1 millisec
+        // usleep(delay);
+        // delay *= 2;  
+        // if (delay > 1000) delay = 1000; // Limit delay to 1 millisec
     }
     return 0;
 }
@@ -49,6 +61,8 @@ void* func() {
 }
 
 int main() {
+    clock_gettime(CLOCK_MONOTONIC, &t_start);
+
     pthread_t thr[nthr];
 
     sem_init(&sem);
@@ -57,10 +71,14 @@ int main() {
         pthread_create(&thr[i], NULL, func, NULL);
     }
 
+    clock_gettime(CLOCK_MONOTONIC, &t_stop);
+    measured_time = seconds(&t_start, &t_stop);
+
     for (int i = 0; i < nthr; i++) {
         pthread_join(thr[i], NULL);
     }
 
+    printf("Part 2 time : %d\n", measured_time);
     printf("counter = %d\n", counter);
 
     return 0;
