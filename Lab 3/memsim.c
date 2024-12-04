5e594dc2714f79eb969cb8d2400fe5ef2c9046be
pottsorken <163292800+pottsorken@users.noreply.github.com>
2024-12-04 15:56:32 +0100

    trying to sunch

--- Lab 3/memsim.c
+++ Lab 3/memsim.c
@@ -60,11 +60,38 @@ memory *create_physical_memory() {
 //the physical address. We then take the physical page number and
 
 
-void convert_virtual_to_physical(){
+void to_binary(int n)
+{
+    int binnum[32];
+    int i = 0;
+    while (n > 0) {
+        binnum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--)
+        printf("%d", binnum[j]);
+}
+
+
+int convert_virtual_to_physical(int va){
     // Take virtual address, check first 8 bits = frame number
     // We take the virtual address mod frame size = page offset
     // Frame number X frame size + offset = physical address
-    llr
+    // llr
+
+
+    //in the page table the page number will be uninitialized and
+    //as we start converting virtual addresses to physical addresses
+    //we will update the page number in the page table
+    //so the first page number we get e.g. 10 will be linked to the
+    //first frame number, 0. The second page number we get e.g. 15
+    //will be linked to the second frame number, 1.
+
+    // to_binary(va);
+    // printf("Binary: %d \n", va);
+    // va = va 
+// printf("a << 1 = %d\n", (a << 1));
 }
 
 
@@ -73,4 +100,5 @@ int main() {
     memory *vm = create_virtual_memory();
     populate_virtual_memory(vm);
     printf("test: %d\n", vm->a[999]);
+    convert_virtual_to_physical(10);
 }
